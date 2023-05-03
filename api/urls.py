4d380685a73dc047e5ccd5a1ef2b354a76767457
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-03 12:13:33 +0100

    Delete unused code

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,6 @@ from . import views
 urlpatterns = [
     path('getRoutes', views.getRoutes, name='getRoutes'),
     path('notes', views.getNotes, name="notes"),
-    # path('notes/<str:pk>/update', views.updateNote, name="update-note"),
-    # path('notes/<str:pk>/delete', views.deleteNote, name="delete-note"),
-    # path('notes/create', views.createNote, name="create-note"),
     path('notes/<str:pk>', views.getNote, name="note")
     
 ]
