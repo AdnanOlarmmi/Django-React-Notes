33fc40bd1d8ccb3e71226cff38535e2000f56fd4
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-11 09:11:06 +0100

    Add User class

--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    USER_ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    user_role = models.CharField(max_length=10, choices=USER_ROLE_CHOICES, default='user')
 
 class Note(models.Model):
     body = models.TextField(null=True, blank=True)
