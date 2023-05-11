64d89d40baedc6a2310eedc813ec7197d0079c9a
AdnanOlarmmi <olarmmi@gmail.com>
2023-05-11 09:31:39 +0100

    Migrate User

--- api/models.py
+++ api/models.py
@@ -9,6 +9,20 @@ class User(AbstractUser):
         ('user', 'User'),
     )
     user_role = models.CharField(max_length=10, choices=USER_ROLE_CHOICES, default='user')
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
 class Note(models.Model):
     body = models.TextField(null=True, blank=True)
